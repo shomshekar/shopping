650a8ac9b301edd4cfe88c5270a621e0e5ba9bd2
shomshekar <shomshekar@rediff.com>
2014-01-22 12:52:01 +0530

    link added for fourth line

--- test.php
+++ test.php
@@ -2,6 +2,6 @@
 <?php echo 'here<br>';
 			echo "second change<br>";
 			echo "Third Change<br>";
-			echo "fourth line<br>";
+			echo "<a href="javascript:void(0);">Fourth Line</a><br>";
 ?>
 </html>
